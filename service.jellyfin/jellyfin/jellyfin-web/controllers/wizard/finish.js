16f4346aec9430fd6d8d1b741d31560248b93761
Nikolay Voronov <nkvoronov@gmail.com>
2020-07-22 23:49:23 +0300

    Update to version 9.2.0 (032)

--- service.jellyfin/jellyfin/jellyfin-web/controllers/wizard/finish.js
+++ service.jellyfin/jellyfin/jellyfin-web/controllers/wizard/finish.js
@@ -1,18 +1,1 @@
-define(["loading"], function (loading) {
-    "use strict";
-
-    function onFinish() {
-        loading.show();
-        ApiClient.ajax({
-            url: ApiClient.getUrl("Startup/Complete"),
-            type: "POST"
-        }).then(function () {
-            loading.hide();
-            window.location.href = "index.html";
-        });
-    }
-
-    return function (view, params) {
-        view.querySelector(".btnWizardNext").addEventListener("click", onFinish);
-    };
-});
+"use strict";define(["loading"],(function(loading){function onFinish(){loading.show(),ApiClient.ajax({url:ApiClient.getUrl("Startup/Complete"),type:"POST"}).then((function(){loading.hide(),window.location.href="index.html"}))}return function(view,params){view.querySelector(".btnWizardNext").addEventListener("click",onFinish)}}));
--- service.jellyfin/jellyfin/jellyfin-web/elements/emby-button/paper-icon-button-light.js
+++ service.jellyfin/jellyfin/jellyfin-web/elements/emby-button/paper-icon-button-light.js
@@ -1,18 +1,1 @@
-define(['layoutManager', 'css!./emby-button', 'registerElement'], function (layoutManager) {
-    'use strict';
-
-    var EmbyButtonPrototype = Object.create(HTMLButtonElement.prototype);
-
-    EmbyButtonPrototype.createdCallback = function () {
-        this.classList.add('paper-icon-button-light');
-
-        if (layoutManager.tv) {
-            this.classList.add('show-focus');
-        }
-    };
-
-    document.registerElement('paper-icon-button-light', {
-        prototype: EmbyButtonPrototype,
-        extends: 'button'
-    });
-});
+"use strict";define(["layoutManager","css!./emby-button","webcomponents"],(function(layoutManager){var EmbyButtonPrototype=Object.create(HTMLButtonElement.prototype);EmbyButtonPrototype.createdCallback=function(){this.classList.add("paper-icon-button-light"),layoutManager.tv&&this.classList.add("show-focus")},document.registerElement("paper-icon-button-light",{prototype:EmbyButtonPrototype,extends:"button"})}));
